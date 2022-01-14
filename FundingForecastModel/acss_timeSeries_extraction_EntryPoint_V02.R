aa701109e3392ab407b42b5a5ac30f6e098155e4
sbewaji-payments <77123572+sbewaji-payments@users.noreply.github.com>
2022-01-14 09:19:15 -0500

    Add files via upload

--- FundingForecastModel/acss_timeSeries_extraction_EntryPoint_V02.R
+++ FundingForecastModel/acss_timeSeries_extraction_EntryPoint_V02.R
@@ -48,22 +48,25 @@ gc();
 
 ##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<IMPORT REQUIRED LIBRARIES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ##The use of require forces R to install the library if not already installed
-require(plyr);# contains method and function calls to be used to filter and select subsets of the data
-require(lubridate);# handles that date elements of the data
-require(dplyr);# contains method and function calls to be used to filter and select subsets of the data
-require(data.table);#used to store and manipiulate data in the form of a data table similar to a mySQL database table
-require(xts);#used to manipulate date-time data into quarters
-require(ggplot2);#used to plot data
-require(zoo);#for date conventions used in financial analysis and quantitiative analysis
-require(magrittr);#contains method/function calls to use for chaining e.g. %>%
-require(scales);#component of ggplot2 that requires to be manually loaded to handle the scaling of various axies
-require(foreach);#handles computations for each element of a vector/data frame/array/etc required the use of %do% or %dopar% operators in base R
-require(iterators);#handles iteration through for each element of a vector/data frame/array/etc
-require(stringr);#handles strings. Will be used to remove all quotation marks from the data
-require(moments);#3rd-party library used to compute statistics such as skewness and kurtosis which are not provided in the base R package set
-require(rlist);#3-party package to allow more flexible and java-collections like handling of lists in R. Allows you to save a list as a Rdata file
-require(foreign);#to export data into formats readable by STATA and other statistical packages
-require(xlsx);#3-party package to export data into excel xlsx format
+##import require libraries 
+##Make the list of packages to be used in this script
+usedPackagesList <- c("lubridate","plyr","dplyr","data.table","ggplot2","magrittr","scales","moments","gghighlight",
+                      "rlist","foreign","zoo","xts","xlsx","gridExtra","grid","lattice","foreach","iterators",
+                      "stringr","ggrepel", "latticeExtra", "forecast", "tseries", "timeSeries", "astsa","TSA"
+);
+
+##check to see if required packages are installed.
+#if not install and load them them
+##else looad them
+checkPagagesInstalled <- lapply(
+  usedPackagesList,
+  FUN = function(x){
+    if(!require(x,character.only = TRUE)){
+      install.packages(x, dependencies = TRUE);
+      require(x,character.only = TRUE)
+    }
+  }
+);
 
 
 
--- FundingForecastModel/acssusbe_timeSeries_analysis_EntryPoint_V03.R
+++ FundingForecastModel/acssusbe_timeSeries_analysis_EntryPoint_V03.R
@@ -32,25 +32,25 @@ rm(list=ls());
 gc();
 
 ##import require libraries 
-require(lubridate);# handles that date elements of the data
-require(dplyr);# contains method and function calls to be used to filter and select subsets of the data
-require(data.table);#used to store and manipiulate data in the form of a data table similar to a mySQL database table
-require(ggplot2);#used to plot data
-require(magrittr);#contains method/function calls to use for chaining e.g. %>%
-require(scales);#component of ggplot2 that requires to be manually loaded to handle the scaling of various axies
-require(moments);#3rd-party library used to compute statistics such as skewness and kurtosis which are not provided in the base R package set
-require(rlist);#3-party package to allow more flexible and java-collections like handling of lists in R. Allows you to save a list as a Rdata file
-require(foreign);#to export data into formats readable by STATA and other statistical packages
-require(xts);#used to manipulate date-time data into quarters
-require(xlsx);#to export data into formats readable by excel
-require(lattice);#to export data into formats readable by excel
-require(latticeExtra);#to export data into formats readable by excel
-require(forecast);#to export data into formats readable by excel
-require(tseries);
-require(timeSeries);
-require(astsa);##Load the astsa package for SARIMA forcasting 
-#require(sarima);##Load the sarima package
-#require(aTSA);
+##Make the list of packages to be used in this script
+usedPackagesList <- c("lubridate","dplyr","data.table","ggplot2","magrittr","scales","moments","gghighlight",
+                      "rlist","foreign","zoo","xts","xlsx","gridExtra","grid","lattice",
+                      "ggrepel", "latticeExtra", "forecast", "tseries", "timeSeries", "astsa","TSA"
+);
+
+##check to see if required packages are installed.
+#if not install and load them them
+##else looad them
+checkPagagesInstalled <- lapply(
+  usedPackagesList,
+  FUN = function(x){
+    if(!require(x,character.only = TRUE)){
+      install.packages(x, dependencies = TRUE);
+      require(x,character.only = TRUE)
+    }
+  }
+);
+
 
 
 ##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DECLARE METHODS/FUNCTIONS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -65,7 +65,7 @@ require(astsa);##Load the astsa package for SARIMA forcasting
 startYear <- 2001;
 endYear <- 2021;
 forecastEndYear <- 2029;
-measurementUnitType <- "Value";##Volume or Value
+measurementUnitType <- "Volume";##Volume or Value
 if(measurementUnitType == "Volume"){
   measurementUnit <- 1000000; #possible values 1000000000, 1000000, 1000, or 1
 }else{
